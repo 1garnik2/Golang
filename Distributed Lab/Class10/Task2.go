f8786c87b653bf8122094e4f3d6f487b4faa1292
Ihor Nikolaiev <1garnik2@gmail.com>
2022-12-02 18:28:35 +0200

    some

--- Distributed Lab/Class10/Task2.go
+++ Distributed Lab/Class10/Task2.go
@@ -9,7 +9,7 @@ func main() {
 	var AC, BC, ACB float64
 	AC = 22
 	BC = 21
-	ACB = 60
+	ACB = 60 * (math.Pi / 3)
 	AB := math.Pow(AC, 2) + math.Pow(BC, 2) - 2*AC*BC*(math.Cos(ACB))
-	fmt.Print(math.Round(AB))
+	fmt.Print(int(AB))
 }
--- Go/test.go
+++ Go/test.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	emoji := []rune("привет")
 
-	for i := 0; i < len(emoji); i++ {
-		fmt.Print(string(emoji[i]), " ") // выводим код символа и его строковое представление
-	}
+	cos := math.Cos(60)
+	fmt.Print((cos))
 }
