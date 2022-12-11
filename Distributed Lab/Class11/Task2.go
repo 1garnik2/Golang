4f23b82281dda4ad2905bc0a8966312fbb5725e7
Ihor Nikolaiev <1garnik2@gmail.com>
2022-12-11 19:22:33 +0200

    add distribLab

--- Distributed Lab/Class11/Task2.go
+++ Distributed Lab/Class11/Task2.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+const Pi = 3.14
+
+type Date struct {
+	Radius int
+	High   int
+}
+type ValueCilindrConus interface {
+	СylinderVolume() float64
+	ConusVolume() float64
+}
+
+func (n Date) СylinderVolume() float64 {
+	return Pi * (math.Pow(float64(n.Radius), 2)) * float64(n.High)
+}
+func (n Date) ConusVolume() float64 {
+	return (Pi * math.Pow(float64(n.Radius), 2) / 3) * float64(n.High)
+}
+func main() {
+	var a ValueCilindrConus
+	numbers := Date{10, 6}
+	a = numbers
+	fmt.Printf("Сylinder volume %.d \n", int(a.СylinderVolume()))
+	fmt.Printf("Conus volume %.d \n", int(a.ConusVolume()))
 }
--- Distributed Lab/Class11/tempCodeRunnerFile.go
+++ Distributed Lab/Class11/tempCodeRunnerFile.go
@@ -1,1 +1,31 @@
-numbers := Date{Square: 12, High: 6}
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const Pi = 3.14
+
+type Date struct {
+	Radius int
+	High   int
+}
+type ValueCilindrConus interface {
+	СylinderVolume() float64
+	ConusVolume() float64
+}
+
+func (n Date) СylinderVolume() float64 {
+	return Pi * (math.Pow(float64(n.Radius), 2)) * float64(n.High)
+}
+func (n Date) ConusVolume() float64 {
+	return (Pi * math.Pow(float64(n.Radius), 2) / 3) * float64(n.High)
+}
+func main() {
+	var a ValueCilindrConus
+	numbers := Date{10, 6}
+	a = numbers
+	fmt.Printf("Сylinder volume %.d \n", int(a.СylinderVolume()))
+	fmt.Printf("Conus volume %.d \n", int(a.ConusVolume()))
+}
